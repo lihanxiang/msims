0e07b004e4119d2d578a1faabbc1c05982e08302
李汉祥 <l945457758@gmail.com>
2019-11-13 12:30:09 +0800

    Update

--- src/main/java/com/lee/msims/mapper/moodle/SubmissionMapper.java
+++ src/main/java/com/lee/msims/mapper/moodle/SubmissionMapper.java
@@ -1,9 +1,10 @@
 package com.lee.msims.mapper.moodle;
 
 import com.lee.msims.pojo.moodle.Submission;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
@@ -15,6 +16,20 @@ public interface SubmissionMapper {
     void createSubmission(Submission submission);
 
     // Update
-    
+    @Update("UPDATE submission SET fileId = #{fileId} AND author = #{author} AND comment = #{comment}")
     void editSubmission(Submission submission);
+
+    // Select
+    @Select("SELECT * FROM submission WHERE id = #{id}")
+    Submission getSubmissionById(int id);
+
+    @Select("SELECT * FROM submission WHERE assignmentId = #{assignmentId}")
+    List<Submission> getSubmissionInAssignment(int assignmentId);
+
+    @Select("SELECT * FROM submission WHERE userId = #{userId}")
+    List<Submission> getSubmissionByUserId(int userId);
+
+    // Delete
+    @Delete("DELETE FROM submission WHERE ")
+    void deleteSubmission(int id);
 }
